d7625b0bf19bbdd9339098a9a4918e0e44cc9034
Mikola Kutsko <kn212_kmm@student.ztu.edu.ua>
2022-11-08 17:07:49 +0200

    Change console app message

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Довільний текст");
+Console.WriteLine("Довільний текст");
+Console.WriteLine("Довільний текст");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
